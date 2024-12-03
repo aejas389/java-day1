676122d527c0a93285f16c43546504e8eb6f89a4
aejas389 <23hr1a3252@mtieat.org>
2024-12-03 14:17:53 +0530

    Add files via upload

---
+++ ashraf7.java
@@ -0,0 +1,23 @@
+public class ashraf7 {
+    public static void main(String args[]){
+
+        int a=2;
+        int b=5;
+
+
+
+        a=a^b;
+        b=a^b;
+        a=a^b;
+
+
+        System.out.println(a);
+        System.out.println(b);
+        
+
+
+
+
+
+    }
+}
