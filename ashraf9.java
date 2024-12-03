9ceebb2228a01439e6fb64fa7cb5f6bec0a63d82
aejas389 <23hr1a3252@mtieat.org>
2024-12-03 16:08:37 +0530

    Add files via upload

---
+++ ashraf9.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class ashraf9 {
+    public static void main(String[] args) 
+    {
+        System.out.println(122^122);
+        System.out.println(130^140);
+        System.out.println(120^130);
+
+    }
+
+}
+
