5bf744b625fa94e04237dd670064bd54d5516134
aejas389 <23hr1a3252@mtieat.org>
2024-12-03 15:50:06 +0530

    Add files via upload

---
+++ ashraf4.java
@@ -0,0 +1,27 @@
+public class ashraf4
+{
+    public static void main(String args)
+    {
+        int a =10;
+        int b =20;
+
+        int c=a+b;
+        //int d=a-b;
+        //int e=a*b;
+        //int f=a/b;
+        //int g=a%b;
+
+        int c = int.format("the sum of %d and % d ",a,b);
+        //int d = int.format("the sum of %d and % d ",a,b);
+        //int e = int.format("the sum of %d and % d ",a,b);
+        //int f = int.format("the sum of %d and % d ",a,b);
+        //int g = int.format("the sum of %d and % d ",a,b); 
+
+        System.out.println(c);
+        //System.out.println(c);
+        //System.out.println(c);
+        //System.out.println(c);
+        //System.out.println(c);
+
+    }
+}
